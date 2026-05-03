a8ffe9789f1dc0f8ec8c21e343894ed46351142f
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-05-03 21:34:08 +0400

    6.3-Anatomy of a POST Request End Point.

--- 06-API-DEVELOPMENT-PART-1/app.js
+++ 06-API-DEVELOPMENT-PART-1/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // app.get("/users/:id", (req, res) => {
 //     const userId = req.params.id;
 //     const filter = req.query.filter;
--- 06-API-DEVELOPMENT-PART-1/user.js
+++ 06-API-DEVELOPMENT-PART-1/user.js
@@ -5,7 +5,21 @@ const router = express.Router();
 router.get("/user/:id", (req, res) => {
     const userId = req.params.id;
     const filter = req.query.filter;
-    res.send(`User ID is ${userId}, Filter is ${filter}`)
+    res.status(200).send(`User ID is ${userId}, Filter is ${filter}`)
 });
+
+
+router.post("/users", (req, res) => {
+    const {name, age} = req.body;
+    if (!name) {
+        return res.status(400).json({ error: "Name is required"})
+    }
+
+    const user = {name, age};
+    res.status(201).json({
+        message: "User created Successfully!",
+        user: user
+    })
+})
 
 module.exports = router;
