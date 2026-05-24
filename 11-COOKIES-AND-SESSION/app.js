e7b2a7897223dd03a67cdda6ec4292354292a27a
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-05-24 22:24:07 +0400

    Custom Session Storage II

--- 11-COOKIES-AND-SESSION/app.js
+++ 11-COOKIES-AND-SESSION/app.js
@@ -7,10 +7,14 @@ app.use(express.urlencoded({ extended: true }));
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
+// SESSION STORAGE
+const sessionStorage = {};
+
 // LOGIN POST ROUTE
 app.post("/login", (req, res) => {
     const {username} = req.body;
     res.cookie("name", username, {maxAge: 900000, httpOnly: true});
+    sessionStorage[username] = { loggedIn: true};
     res.send("Cookie is set");
 });
 
@@ -18,7 +22,7 @@ app.post("/login", (req, res) => {
 // PROTECTED ROUTE
 app.get("/protected", (req, res) => {
     const {name} = req.cookies;
-    if (name == "Shahjalal") {
+    if (sessionStorage[name] && sessionStorage[name].loggedIn) {
         res.send(`Hello, ${name}. you have access to the protected route!`);
     } else {
         res.status(401).send("Unauthorized")
