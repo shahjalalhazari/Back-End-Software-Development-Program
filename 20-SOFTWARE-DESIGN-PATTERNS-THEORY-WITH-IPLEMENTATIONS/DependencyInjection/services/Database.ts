559396d2f3fe35fc13aa31a112b63760c849dec0
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-07-14 14:46:03 +0400

    20.2.4-Testing and DI - How it helps to test software.

--- 20-SOFTWARE-DESIGN-PATTERNS-THEORY-WITH-IPLEMENTATIONS/DependencyInjection/services/Database.ts
+++ 20-SOFTWARE-DESIGN-PATTERNS-THEORY-WITH-IPLEMENTATIONS/DependencyInjection/services/Database.ts
@@ -3,7 +3,7 @@ interface Database {
     query(sql: string): any;
 }
 
-
+// CONNECTION FOR POSTGRESQL
 class PostgresDatabase implements Database{
     connect() {
         console.log("Connected to Postgres");
@@ -14,7 +14,7 @@ class PostgresDatabase implements Database{
     }
 };
 
-
+// CONNECTION FOR MONGODB
 class MongoDatabase implements Database {
     connect() {
         console.log("Connected to MongoDB");
@@ -25,4 +25,22 @@ class MongoDatabase implements Database {
     }
 }
 
-export {Database, PostgresDatabase, MongoDatabase};
+
+// FAKE DATABASE CONNECTOR
+class FakeDatabase implements Database{
+    connect(): void {
+        console.log("[FAKE-DB] Pretending to connect...");
+    }
+
+    query(sql: string) {
+        console.log("[FAKE-DB] query:", sql);
+        // RETURN FAKE USER DATA
+        return [
+            {id: 1, name: "Shahjalal Hazari"},
+            {id: 2, name: "Emon Hazari"},
+            {id: 3, name: "Rubayet Islam"}
+        ]
+    }
+}
+
+export {Database, PostgresDatabase, MongoDatabase, FakeDatabase};
--- 20-SOFTWARE-DESIGN-PATTERNS-THEORY-WITH-IPLEMENTATIONS/DependencyInjection/services/UserControllers.ts
+++ 20-SOFTWARE-DESIGN-PATTERNS-THEORY-WITH-IPLEMENTATIONS/DependencyInjection/services/UserControllers.ts
@@ -1,6 +1,11 @@
-import { MongoDatabase, PostgresDatabase } from "./Database";
+import { FakeDatabase, MongoDatabase, PostgresDatabase } from "./Database";
 import UserService from "./UserService";
 
 
 const userServices = new UserService(new MongoDatabase());
 const userServicesTwo = new UserService(new PostgresDatabase());
+
+
+const fakeUserServices = new UserService(new FakeDatabase());
+const fakeUsers = fakeUserServices.getUsers();
+console.log(fakeUsers);
