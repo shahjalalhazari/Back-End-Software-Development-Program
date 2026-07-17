54c985593b3d4e337da4e06d7b9395407cce2adf
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-07-17 22:21:19 +0400

    21.6-Modules In NestJS

---
+++ 21-NESTJS-BUILDING-ENTERPRISE-APPLICATIONS/membership/src/cat/cat.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CatController } from './cat.controller';
+import { CatService } from './cat.service';
+
+@Module({
+    controllers: [CatController],
+    providers: [CatService],
+    exports: [CatService]
+})
+
+export class CatsModule {}
