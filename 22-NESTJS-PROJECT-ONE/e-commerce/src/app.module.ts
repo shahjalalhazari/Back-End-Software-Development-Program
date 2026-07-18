8dcf564741b8a30ba49c2bfb8297bcf012934481
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-07-18 19:54:37 +0400

    22.1.5-Connecting Database.

--- 22-NESTJS-PROJECT-ONE/e-commerce/src/app.module.ts
+++ 22-NESTJS-PROJECT-ONE/e-commerce/src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
