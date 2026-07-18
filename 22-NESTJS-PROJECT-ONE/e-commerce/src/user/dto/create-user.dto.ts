2a054002c1783573086c2d13feb91190ba7ed6a0
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-07-18 22:16:26 +0400

    22.1.7-API for super admins. Project Reqwuirement.

---
+++ 22-NESTJS-PROJECT-ONE/e-commerce/src/user/dto/create-user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  role?: UserRole;
+}
--- 22-NESTJS-PROJECT-ONE/e-commerce/src/user/user.service.ts
+++ 22-NESTJS-PROJECT-ONE/e-commerce/src/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User, UserRole } from './entities/user.entity';
+
+type SafeUser = Omit<User, 'password'>;
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(data: CreateUserDto): Promise<SafeUser> {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: data.email },
+    });
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = this.userRepository.create({
+      email: data.email,
+      password: hashedPassword,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      role: data.role ?? UserRole.CUSTOMER,
+    });
+
+    const savedUser = await this.userRepository.save(user);
+    const { password, ...safeUser } = savedUser;
+
+    return safeUser;
+  }
+}
