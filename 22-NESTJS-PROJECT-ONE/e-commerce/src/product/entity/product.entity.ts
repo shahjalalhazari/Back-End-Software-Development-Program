5b9eaca3d58cec0fdfce2ebd9740f3e81b4fa8ad
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-08-02 16:23:11 +0400

    Product entity has been updated.

--- 22-NESTJS-PROJECT-ONE/e-commerce/src/product/entity/product.entity.ts
+++ 22-NESTJS-PROJECT-ONE/e-commerce/src/product/entity/product.entity.ts
@@ -1,9 +1,12 @@
+import { Store } from 'src/store/entity/store.entity';
 import {
   Column,
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
   Index,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -15,49 +18,62 @@ export enum ProductStatus {
   HIDDEN = 'HIDDEN',
 }
 
-
 @Entity('products')
 export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  @Index()
+  storeId: string;
+
+  @ManyToOne(() => Store, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'storeId' })
+  store: Store;
 
   @Column()
   @Index()
   name: string;
 
-
   @Column({ type: 'text' })
   description: string;
-
 
   @Column({ unique: true })
   @Index()
   sku: string;
 
-
   @Column({ unique: true, nullable: true })
+  @Index()
   slug?: string;
-
 
   @Column({
     type: 'decimal',
     precision: 10,
     scale: 2,
+    transformer: {
+      to: (value: number) => value,
+      from: (value: string) => Number(value),
+    }
   })
+  @Index()
   price: number;
-
 
   @Column({
     type: 'decimal',
+    precision: 10,
+    scale: 2,
     nullable: true,
+    transformer: {
+      to: (value: number) => value,
+      from: (value: string) => Number(value),
+    }
   })
   compareAtPrice?: number;
 
-
   @Column({ default: true })
   trackInventory: boolean;
-
 
   @Column({
     type: 'int',
@@ -65,14 +81,13 @@ export class Product {
   })
   stock: number;
 
-
   @Column({
     type: 'enum',
     enum: ProductStatus,
     default: ProductStatus.DRAFT,
   })
+  @Index()
   status: ProductStatus;
-
 
   @Column({
     type: 'timestamp',
@@ -80,14 +95,12 @@ export class Product {
   })
   publishedAt?: Date;
 
-
   @CreateDateColumn()
+  @Index()
   createdAt: Date;
-
 
   @UpdateDateColumn()
   updatedAt: Date;
-
 
   @DeleteDateColumn()
   deletedAt?: Date;
